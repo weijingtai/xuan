25acf32c402125f24cfd352b4c2c899954e54715
wjt <wjt19951215@gmail.com>
2024-12-02 17:27:21 -0800

    macos need apple dev account to using google_fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,22 @@ import 'package:timezone/data/latest.dart' as tz;
 
 import 'NavigatorGenerator.dart';
 
-void main() async {
-  await initSweph([
-    'packages/sweph/assets/ephe/sefstars.txt', // For star position
-  ]);
-  WidgetsFlutterBinding.ensureInitialized();
+Future<void> initServices() async {
+  // 在这里可以进行其他异步初始化操作
+  // 例如加载配置文件等
+
   tz.initializeTimeZones();
   if (kIsWeb) {
     usePathUrlStrategy();
   }
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await initSweph([
+    'packages/sweph/assets/ephe/sefstars.txt', // For star position
+  ]);
+}
+
+void main() async {
   // runApp(
   //   MultiProvider(
   //     providers: [
@@ -30,7 +37,9 @@ void main() async {
   //     child: const MyApp(),
   //   ),
   // );
-  runApp(const MyApp());
+  initServices().then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -59,7 +68,8 @@ class MyApp extends StatelessWidget {
       // onGenerateRoute: NavigatorGenerator.generateRoute,
     );
   }
-  Widget buildNewXuan(){
+
+  Widget buildNewXuan() {
     return MaterialApp(
       title: '玄学',
       theme: ThemeData(
@@ -94,18 +104,25 @@ class MyApp extends StatelessWidget {
               // Wrap the page with the ResponsiveScaledBox for desired pages.
               //   final Uri uri = Uri.parse(settings.name ?? '/');
               return ConditionalRouteWidget(
-                  routesExcluded: const [
-                  ], // Excluding a page from AutoScale.
-                  builder: (context, child) => child!=null?child:ResponsiveScaledBox(
-                    // ResponsiveScaledBox renders its child with a FittedBox set to the `width` value.
-                    // Set the fixed width value based on the active breakpoint.
-                      width: ResponsiveValue<double>(context, conditionalValues: [
-                        // const Condition.equals(name: MOBILE, value: 450),
-                        const Condition.between(start: 0, end: 450, value: 0),
-                        const Condition.between(start: 800, end: 1100, value: 800),
-                        Condition.between(start: 1000, end: double.maxFinite.toInt(), value: 1000),
-                      ]).value,
-                      child: child!),
+                  routesExcluded: const [], // Excluding a page from AutoScale.
+                  builder: (context, child) => child != null
+                      ? child
+                      : ResponsiveScaledBox(
+                          // ResponsiveScaledBox renders its child with a FittedBox set to the `width` value.
+                          // Set the fixed width value based on the active breakpoint.
+                          width: ResponsiveValue<double>(context,
+                              conditionalValues: [
+                                // const Condition.equals(name: MOBILE, value: 450),
+                                const Condition.between(
+                                    start: 0, end: 450, value: 0),
+                                const Condition.between(
+                                    start: 800, end: 1100, value: 800),
+                                Condition.between(
+                                    start: 1000,
+                                    end: double.maxFinite.toInt(),
+                                    value: 1000),
+                              ]).value,
+                          child: child!),
                   child: BouncingScrollWrapper.builder(
                       context, buildPageByName(uri.path),
                       dragWithMouse: true));
@@ -127,6 +144,7 @@ class MyApp extends StatelessWidget {
       // onGenerateRoute: NavigatorGenerator.generateRoute,
     );
   }
+
   Widget buildPageByName(String name) {
     switch (name) {
       case '/':
@@ -140,6 +158,7 @@ class MyApp extends StatelessWidget {
         return const SizedBox.shrink();
     }
   }
+
   Route<dynamic> buildPage(
       {required String path, Map<String, String> queryParams = const {}}) {
     return Routes.noAnimation(
@@ -147,22 +166,23 @@ class MyApp extends StatelessWidget {
             name: (path.startsWith('/') == false) ? '/$path' : path),
         builder: (context) {
           String pathName =
-          path != '/' && path.startsWith('/') ? path.substring(1) : path;
+              path != '/' && path.startsWith('/') ? path.substring(1) : path;
           return switch (pathName) {
             // '/' || ListPage.name => const ListPage(),
             "ok" =>
-            // Breakpoints can be nested.
-            // Here's an example of custom "per-page" breakpoints.
-            ResponsiveBreakpoints(breakpoints: [
-              Breakpoint(start: 0, end: 480, name: MOBILE),
-              Breakpoint(start: 481, end: 1200, name: TABLET),
-              Breakpoint(start: 1201, end: double.infinity, name: DESKTOP),
-            ], child: RootPage()),
-          '/' || CrossPlatformMainPage.routeName => const CrossPlatformMainPage(),
+              // Breakpoints can be nested.
+              // Here's an example of custom "per-page" breakpoints.
+              ResponsiveBreakpoints(breakpoints: [
+                Breakpoint(start: 0, end: 480, name: MOBILE),
+                Breakpoint(start: 481, end: 1200, name: TABLET),
+                Breakpoint(start: 1201, end: double.infinity, name: DESKTOP),
+              ], child: RootPage()),
+            '/' ||
+            CrossPlatformMainPage.routeName =>
+              const CrossPlatformMainPage(),
             // TypographyPage.name => const TypographyPage(),
             _ => const SizedBox.shrink(),
           };
         });
   }
 }
-
