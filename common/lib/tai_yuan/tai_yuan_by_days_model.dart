6467eb0f8852405cc41321c1c9ad76095cacf033
wjt <wjt19951215@gmail.com>
2025-05-31 00:29:36 -0700

    完成七政四余命盘center circle text功能开发

--- common/lib/tai_yuan/tai_yuan_by_days_model.dart
+++ common/lib/tai_yuan/tai_yuan_by_days_model.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../enums/enum_jia_zi.dart';
+import 'enum_calculate_strategy.dart';
 import 'tai_yuan_model.dart';
 import '../models/eight_chars.dart';
 import '../models/jie_qi_info.dart';
