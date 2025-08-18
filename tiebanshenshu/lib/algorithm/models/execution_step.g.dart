f067d370094fb571d68a6bc13bdfa8e56451dd2c
wjt <wjt19951215@gmail.com>
2025-08-17 22:40:06 -0700

    fix json ser error

---
+++ tiebanshenshu/lib/algorithm/models/execution_step.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'execution_step.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ExecutionStep _$ExecutionStepFromJson(Map<String, dynamic> json) =>
+    ExecutionStep(
+      id: json['id'] as String,
+      name: json['name'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+      operationId: json['operationId'] as String,
+      config: json['config'] as Map<String, dynamic>? ?? {},
+      inputs: json['inputs'] as Map<String, dynamic>? ?? {},
+      outputs: json['outputs'] as Map<String, dynamic>? ?? {},
+      conditionalBranches: (json['conditionalBranches'] as List<dynamic>?)
+          ?.map((e) => ConditionalBranch.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      required: json['required'] as bool? ?? true,
+      timeoutMs: (json['timeoutMs'] as num?)?.toInt(),
+      retryCount: (json['retryCount'] as num?)?.toInt() ?? 0,
+      dependencies:
+          (json['dependencies'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          [],
+      isOptional: json['isOptional'] as bool? ?? false,
+    );
+
+Map<String, dynamic> _$ExecutionStepToJson(ExecutionStep instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'description': instance.description,
+      'operationId': instance.operationId,
+      'config': instance.config,
+      'inputs': instance.inputs,
+      'outputs': instance.outputs,
+      'conditionalBranches': instance.conditionalBranches
+          ?.map((e) => e.toJson())
+          .toList(),
+      'required': instance.required,
+      'timeoutMs': instance.timeoutMs,
+      'retryCount': instance.retryCount,
+      'dependencies': instance.dependencies,
+      'isOptional': instance.isOptional,
+    };
