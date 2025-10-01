601674ae00113c9d22734c0008793ea71276b948
wjt <wjt19951215@gmail.com>
2025-09-30 19:10:45 -0700

    update main to daliuren

--- lib/main.dart
+++ lib/main.dart
@@ -130,9 +130,9 @@ class MyApp extends StatelessWidget {
       // initialRoute: '/one_year',
       // initialRoute: '/dev', // 七政四余
       // initialRoute: '/common/dev', // 占测记录
-      initialRoute: '/qizhengsiyu/panel', // 七政四余
+      // initialRoute: '/qizhengsiyu/panel', // 七政四余
       // initialRoute: '/taiyishenshu', // 太乙神数
-      // initialRoute: '/daliuren/dev', // 大六壬
+      initialRoute: '/daliuren/dev', // 大六壬
       // initialRoute: '/qimendunjia', // 奇门遁甲
       // initialRoute: '/', // main
       // initialRoute: '/widget_dev', // 奇门遁甲
