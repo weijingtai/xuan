5e5ae8980171db699d4921efa8bedf9ae1bc88be
wjt <wjt19951215@gmail.com>
2026-01-30 15:07:08 -0800

    fix: resolve invalid override error in BasicInfoSection.onDataLoad

--- qizhengsiyu/lib/presentation/widgets/config/basic_info_section.dart
+++ qizhengsiyu/lib/presentation/widgets/config/basic_info_section.dart
@@ -666,7 +666,7 @@ class _BasicInfoSectionState extends State<BasicInfoSection>
   }
 
   @override
-  Future<List<AddressNode>> onDataLoad(int index, String code, String name) {
+  Future<List<AddressNode>> onDataLoad(int index, AddressNode? node) {
     throw UnimplementedError("not imp");
   }
 
